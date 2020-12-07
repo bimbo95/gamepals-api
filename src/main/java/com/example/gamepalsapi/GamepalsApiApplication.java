bea8bea35d6f20deea6a6b6afd1bdee8d37a8320
Julius Ivoska <julius.ivoska@gmail.com>
2020-12-07 19:32:23 +0200

    initial commit

---
+++ src/main/java/com/example/gamepalsapi/GamepalsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamepalsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamepalsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamepalsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gamepalsapi/GamepalsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamepalsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamepalsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
